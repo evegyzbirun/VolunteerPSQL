8e02124d9d1dc798fea38aa39e104d792902577c
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-08 14:59:02 -0700

    added update, find, delete methods in project rb.

--- lib/project.rb
+++ lib/project.rb
@@ -24,5 +24,34 @@ class Project
     @id = result.first().fetch("id").to_i
   end
 
-  
+  def ==(project_to_compare)
+    self.name() == project_to_compare.name()
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM projects *;")
+  end
+
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    name = project.fetch("name")
+    id = project.fetch("id").to_i
+    Project.new({:name => name, :id => id})
+  end
+
+  def update(name)
+    if (attributes.has_key?(:name)) && (attributes.fetch(:name) != nil)
+      @name = attributes.fetch(:name)
+      DB.exec("UPDATE projects SET name = '#{@name}' WHERE id = #{@id};")
+    end
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{@id};") 
+  end
+
+  # def volunteers 
+  #   Volunteer.find_by_project(self.id)
+  # end
 end 
