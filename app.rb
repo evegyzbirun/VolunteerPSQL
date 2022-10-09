780c7b111b8dd66aa95d4c42f1f72598aa8cc6f8
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-09 08:32:33 -0700

    added project into app.rb

--- app.rb
+++ app.rb
@@ -11,6 +11,44 @@ also_reload('lib/**/*.rb')
 DB = PG.connect({:dbname => "volunteer_tracker", :password => ENV['DATABASE_PASS']})
 
 get('/') do
-  @projects = Album.all
-  erb(:albums)
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:project_new)
+end
+
+post('/projects') do
+  name = params[:project_name]
+  project = Project.new({:name => name, :id => nil})
+  project.save()
+  redirect to('/projects')
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:name])
+  redirect to('/projects')
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  redirect to('/projects')
 end
