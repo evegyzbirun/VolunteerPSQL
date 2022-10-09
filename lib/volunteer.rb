2590d78c6f89c3e4d7a0974bf77d728eb1b2eee5
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-08 18:51:29 -0700

    added == and all.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -9,5 +9,23 @@ class Volunteer
     @id = attributes.fetch(:id)
   end
 
-  
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+      (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+    else
+      false
+    end
+  end
 end
